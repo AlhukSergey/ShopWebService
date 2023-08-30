cc30ca32af8291e2af9eaf130ea3e902723e71e0
AlhukSergey <alshuk73@gmail.com>
2023-08-30 05:40:54 +0300

    Finished the swagger task.

--- src/main/java/by/teachmeskills/shopwebservice/controllers/CategoryController.java
+++ src/main/java/by/teachmeskills/shopwebservice/controllers/CategoryController.java
@@ -2,6 +2,14 @@ package by.teachmeskills.shopwebservice.controllers;
 
 import by.teachmeskills.shopwebservice.dto.CategoryDto;
 import by.teachmeskills.shopwebservice.services.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +23,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
+@Tag(name = "category", description = "Category Endpoint")
 @RestController
 @RequestMapping("/category")
 public class CategoryController {
@@ -25,28 +35,107 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
+    @Operation(
+            summary = "Find all categories",
+            description = "Find all existed categories in Shop",
+            tags = {"category"}
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "All categories were found",
+                            content = @Content(array = @ArraySchema(schema = @Schema(implementation = CategoryDto.class)))
+                    ),
+                    @ApiResponse(
+                            responseCode = "400",
+                            description = "Categories not found - server error"
+                    )
+            }
+    )
     @GetMapping("/all")
     public ResponseEntity<List<CategoryDto>> getAllCategories() {
         return new ResponseEntity<>(categoryService.getAllCategories(), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Find certain category",
+            description = "Find certain existed category in Shop by its id",
+            tags = {"category"})
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Category was found by its id",
+                    content = @Content(schema = @Schema(contentSchema = CategoryDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Category not fount - forbidden operation"
+            )
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<CategoryDto> getCategoryById(@PathVariable int id) {
-        return new ResponseEntity<>(categoryService.getCategory(id), HttpStatus.OK);
+    public ResponseEntity<CategoryDto> getCategoryById(@Parameter(required = true, description = "Category ID") @PathVariable int id) {
+        return Optional.ofNullable(categoryService.getCategory(id))
+                .map(dto -> new ResponseEntity<>(dto, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @Operation(
+            summary = "Create category",
+            description = "Create new category",
+            tags = {"category"})
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Category was created",
+                    content = @Content(schema = @Schema(contentSchema = CategoryDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Category not created - server error"
+            )
+    })
     @PostMapping
     public ResponseEntity<CategoryDto> createCategory(@RequestBody @Valid CategoryDto categoryDto) {
         return new ResponseEntity<>(categoryService.createCategory(categoryDto), HttpStatus.CREATED);
     }
 
+    @Operation(
+            summary = "Update category",
+            description = "Update existed category",
+            tags = {"category"})
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Category was updated",
+                    content = @Content(schema = @Schema(contentSchema = CategoryDto.class))
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Category not updated - server error"
+            )
+    })
     @PutMapping
     public ResponseEntity<CategoryDto> updateCategory(@RequestBody @Valid CategoryDto categoryDto) {
         return new ResponseEntity<>(categoryService.updateCategory(categoryDto), HttpStatus.OK);
     }
 
+    @Operation(
+            summary = "Delete category",
+            description = "Delete existed category",
+            tags = {"category"})
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Category was deleted"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Category not deleted - server error"
+            )
+    })
     @DeleteMapping("/{id}")
-    public void deleteCategory(@PathVariable int id) {
+    public void deleteCategory(@Parameter(required = true, description = "Category ID") @PathVariable int id) {
         categoryService.deleteCategory(id);
     }
 }
